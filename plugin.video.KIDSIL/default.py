0a9f74309543769bf940cb54c4238f78e35c15bf
o2ri <mamitj@gmail.com>
2013-10-21 20:52:52 +0200

    test

--- plugin.video.KIDSIL/default.py
+++ plugin.video.KIDSIL/default.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib,urllib2,sys,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc
+import json
 __settings__ = xbmcaddon.Addon(id='plugin.video.KIDSIL')
 ADDON = xbmcaddon.Addon(id='plugin.video.KIDSIL')
 def CATEGORIES():
@@ -14,8 +15,9 @@ def CATEGORIES():
                 addDir('[COLOR red]seretil לא מותקן[/COLOR]','','','','')
         if os.path.exists(xbmc.translatePath("special://home/addons/") + 'plugin.video.movie25'):
                 
-                addDir('ללא דיבוב','plugin://plugin.video.movie25/?fanart=https%3a%2f%2fgithub.com%2fmash2k3%2fMashupArtwork%2fraw%2fmaster%2fart%2ffanart2.jpg&iconimage=https%3a%2f%2fencrypted-tbn3.gstatic.com%2fimages%3fq%3dtbn%3aANd9GcTR26WavA0VthRpyIneD6ERay2rnWOA5gxoWnfTDCfAWCfHcXg6&mode=236&name=Animated%20Movies%20%5bCOLOR%20red%5d%20Updated%2009%2f09%2f13%5b%2fCOLOR%5d&plot&url=https%3a%2f%2fgithub.com%2fmash2k3%2fStaael1982%2fraw%2fmaster%2fanimated_movies.xml',8,'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTR26WavA0VthRpyIneD6ERay2rnWOA5gxoWnfTDCfAWCfHcXg6','')
-                addDir('ללא דיבוב 2','plugin://plugin.video.movie25/?fanart=http%3a%2f%2fs20.postimg.org%2ff1ov06599%2ffanart2.png&amp;iconimage=http%3a%2f%2fs20.postimg.org%2fsx4lyuvbx%2fkidzone.png&amp;mode=236&amp;name=KidZonE%20%5bCOLOR%20red%5d%20Updated%2017%2f09%2f13%5b%2fCOLOR%5d&amp;plot&amp;url=https%3a%2f%2fgithub.com%2fmash2k3%2fMashUpTNPB%2fraw%2fmaster%2fkidszone.xml',8,'http://s20.postimg.org/sx4lyuvbx/kidzone.png','')
+                addDir('staael1982 (mashup) ללא דיבוב','plugin://plugin.video.movie25/?fanart=https%3a%2f%2fgithub.com%2fmash2k3%2fMashupArtwork%2fraw%2fmaster%2fart%2ffanart2.jpg&iconimage=https%3a%2f%2fencrypted-tbn3.gstatic.com%2fimages%3fq%3dtbn%3aANd9GcTR26WavA0VthRpyIneD6ERay2rnWOA5gxoWnfTDCfAWCfHcXg6&mode=236&name=Animated%20Movies%20%5bCOLOR%20red%5d%20Updated%2009%2f09%2f13%5b%2fCOLOR%5d&plot&url=https%3a%2f%2fgithub.com%2fmash2k3%2fStaael1982%2fraw%2fmaster%2fanimated_movies.xml',8,'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTR26WavA0VthRpyIneD6ERay2rnWOA5gxoWnfTDCfAWCfHcXg6','')
+                #addDir('ללא דיבוב 2','plugin://plugin.video.movie25/?fanart=http%3a%2f%2fs20.postimg.org%2ff1ov06599%2ffanart2.png&amp;iconimage=http%3a%2f%2fs20.postimg.org%2fsx4lyuvbx%2fkidzone.png&amp;mode=236&amp;name=KidZonE%20%5bCOLOR%20red%5d%20Updated%2017%2f09%2f13%5b%2fCOLOR%5d&amp;plot&amp;url=https%3a%2f%2fgithub.com%2fmash2k3%2fMashUpTNPB%2fraw%2fmaster%2fkidszone.xml',8,'http://s20.postimg.org/sx4lyuvbx/kidzone.png','')
+                #addDir('ILIVE in Mash','plugin://plugin.video.movie25/?iconimage=https://github.com/mash2k3/MashupArtwork/raw/master/art/ilive.png&amp;mode=120&amp;name=Animation&amp;url=animation&quot',8,'','')
         else:
                addDir('[COLOR red]MASHUP לא מותקן[/COLOR]','','','','')
                
@@ -38,8 +40,10 @@ def CATEGORIES():
                  addDir('[COLOR red]GOZLAN לא מותקן[/COLOR]','','','','')
         addDir(' KIDS LIVE TV (benny 123)','https://dl.dropboxusercontent.com/u/94071174/Online/wow/Kids.plx',7,'http://www.livestream.com/filestore/logos/6a941358-6c7f-2ebf-e8ac-b05f4f338270-banner.png','')
         addDir('Baby Einstein','TerrapinStation5',9,'http://d202m5krfqbpi5.cloudfront.net/books/1170326163l/46377.jpg','1')
+        addDir(' וידאו לילדים','UCnToIWbMbc9VehbtjTBBnRw',9,'http://www.iandroidil.net/icone/5718-icon.png','1')
         YOUsubs('UC5RJ8so5jivihrnHB5qrV_Q')
-        setView('movies', 'default')       
+	#ListPlaylist(playlists[0][0])
+	setView('movies', 'default')       
 
 def update_view(url):
 
@@ -95,7 +99,7 @@ def addDir(name,url,mode,iconimage,description):
         if mode==8:
                 ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
         else:
-                if mode==11:
+                if mode==11 or mode==12:
                         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
                 else:
                         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
@@ -114,18 +118,18 @@ def addLink(name,url,iconimage,description):
 def YOUsubs(user):
       murl='http://gdata.youtube.com/feeds/api/users/'+user+'/subscriptions?start-index=1&max-results=50'
       link=OPEN_URL(murl)
-      print link
       match=re.compile('>Activity of:(.*?)</title>.*?http://gdata.youtube.com/feeds/api/users/(.*?).?/>').findall(link)
-      print """""" + str ( match)
         
       for name ,user in match:
               addDir(name.strip(),user.strip(),9,'http://img-ipad.lisisoft.com/imgmic/1/2/1253-1-youtube-kids.jpg','1')
       setView('tvshows', 'default')
 #list the links from  usernames based on mash23 + improvment
 def YOUList(name,url,description):
+        playlists=PlaylistsFromUser(url)
+        for playlistid,title,thumb in playlists :
+                addDir('[COLOR yellow]Playlist:[/COLOR]                  ' + title,playlistid,12,thumb,'')
+        
         murl='http://gdata.youtube.com/feeds/api/users/'+url+'/uploads?&max-results=50&start-index='+description
-
-        print "murl is :::::::::::::" + str (murl)
         link=OPEN_URL(murl)
         addDir('[COLOR red]נגן כל התוצאות בעמוד זה [/COLOR]',murl,11,'',description)
         match=re.compile("http\://www.youtube.com/watch\?v\=([^\&]+)\&.+?<media\:descriptio[^>]+>([^<]+)</media\:description>.+?<media\:thumbnail url='([^']+)'.+?<media:title type='plain'>(.+?)/media:title>").findall(link)
@@ -133,16 +137,70 @@ def YOUList(name,url,description):
                 rname=rname.replace('<','')
                 YOULink(rname,nurl,thumb)
         description=int(description)+50
-        addDir(' עוד תוצאות',url,9,'',str(description))
+        addDir('[COLOR blue]            עוד תוצאות [/COLOR]',url,9,'',str(description))
 
 def YOULink(mname,url,thumb):
         ok=True
-        url = "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid="+url+"&hd=1"
+        url = "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid="+url
         liz=xbmcgui.ListItem(mname, iconImage="DefaultVideo.png", thumbnailImage=thumb)
         liz.setInfo( type="Video", infoLabels={ "Title": mname, "Plot": description } )
         liz.setProperty("IsPlayable","true")
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=False)
         return ok
+def PlaylistsFromUser(user):
+	url='https://gdata.youtube.com/feeds/api/users/'+user+ '/playlists?alt=json&max-results=50'
+	req = urllib2.Request(url)
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+	prms=json.loads(link)
+	TotalPlaylists=int(prms['feed'][u'openSearch$totalResults'][u'$t'])
+	i=0
+	lst=[]
+	while i<TotalPlaylists:
+		playlistid=str(prms['feed'][u'entry'][i][u'yt$playlistId'][u'$t'])
+		title=str(prms['feed'][u'entry'][i][u'title'][u'$t'].encode('utf-8'))
+		thumb=str(prms['feed'][u'entry'][i][u'media$group'][u'media$thumbnail'][2][u'url'])
+		i=i+1
+		lst.append((playlistid,title,thumb))
+	return lst
+
+'''
+playlist = self.xbmc.PlayList(self.xbmc.PLAYLIST_VIDEO)
+        playlist.clear()
+
+        video_url = "%s?path=/root&action=play_video&videoid=%s"
+        # queue all entries
+        for entry in result:
+            video = entry.get
+            if video("videoid") == "false":
+                continue
+            listitem = self.xbmcgui.ListItem(label=video("Title"), iconImage=video("thumbnail"), thumbnailImage=video("thumbnail"))
+            listitem.setProperty('IsPlayable', 'true')
+            listitem.setProperty("Video", "true" )
+            listitem.setInfo(type='Video', infoLabels=entry)
+
+            playlist.add(video_url % (sys.argv[0], video("videoid") ), listitem)
+
+        if (get("shuffle")):
+            playlist.shuffle()
+
+        self.xbmc.executebuiltin('playlist.playoffset(video , 0)')'''
+
+
+
+def ListPlaylist(playlistid):
+    murl='https://gdata.youtube.com/feeds/api/playlists/'+playlistid+'?max-results=50'
+    link=OPEN_URL(murl)
+    #addDir('[COLOR red]נגן כל התוצאות בעמוד זה [/COLOR]',murl,11,'',description)
+    match=re.compile("http\://www.youtube.com/watch\?v\=([^\&]+)\&.+?<media\:descriptio[^>]+>([^<]+)</media\:description>.+?<media\:thumbnail url='([^']+)'.+?<media:title type='plain'>(.+?)/media:title>").findall(link)
+    for nurl,desc,thumb,rname in match:
+                rname=rname.replace('<','')
+                addLink(rname,nurl,thumb,'')
+    #description=int(description)+50
+    #addDir(' עוד תוצאות',url,9,'',str(description))
+	
 
 def YOULinkAll(url):
     dp = xbmcgui.DialogProgress()
@@ -174,11 +232,65 @@ def YOULinkAll(url):
                     pl.add(blob,liz)
             except:
                 pass
-    print pl
+    
     if not xbmc.Player().isPlayingVideo():
 	    xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(pl)
         
-#below tells plugin about the views                
+def PlayPlayList(playlistid):
+
+        #motek='PLN0EJVTzRDL_eN1W_VnJmcRIItC0epPl7'
+        url='https://gdata.youtube.com/feeds/api/playlists/'+playlistid+'?alt=json&max-results=50'
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        prms=json.loads(link)
+
+        pl = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+        pl.clear()
+        playlist = []
+        numOfItems=int(prms['feed'][u'openSearch$totalResults'][u'$t']) #if bigger than 50 needs  to add more result
+        
+        j=1
+        h=1
+        pages = (numOfItems //50)+1
+        while  j<= pages:
+                link=OPEN_URL(url)
+                prms=json.loads(link)
+                i=0
+                while i< 50  and  h<numOfItems :
+                        urlPlaylist= str(prms['feed'][u'entry'][i][ u'media$group'][u'media$player'][0][u'url'])
+                        match=re.compile('www.youtube.com/watch\?v\=(.*?)\&f').findall(urlPlaylist)
+                        finalurl="plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid="+match[0]+"&hd=1"
+                        title= str(prms['feed'][u'entry'][i][ u'media$group'][u'media$title'][u'$t'].encode('utf-8')).decode('utf-8')
+                        thumb =str(prms['feed'][u'entry'][i][ u'media$group'][u'media$thumbnail'][2][u'url'])
+                        liz = xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+                        liz.setInfo( type="Video", infoLabels={ "Title": title} )
+                        liz.setProperty("IsPlayable","true")
+                        playlist.append((finalurl ,liz))
+                        i=i+1
+                        h=h+1
+
+                j=j+1
+                url='https://gdata.youtube.com/feeds/api/playlists/'+playlistid+'?alt=json&max-results=50&start-index='+str (j*50-49)
+        for blob ,liz in playlist:
+            try:
+                if blob:
+                    pl.add(blob,liz)
+            except:
+                pass
+        
+        if not xbmc.Player().isPlayingVideo():
+	    xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(pl)
+        
+
+ #https://gdata.youtube.com/feeds/api/users/polosoft/playlists (gets playlist fro, user) https://gdata.youtube.com/feeds/api/users/polosoft/playlists?alt=json
+ #https://gdata.youtube.com/feeds/api/playlists/PLN0EJVTzRDL_53Jz8bhZl4m3UtkY2btbV?max-results=50?alt=json  (gets items in playlist)
+ #https://gdata.youtube.com/feeds/api/playlists/PLN0EJVTzRDL_53Jz8bhZl4m3UtkY2btbV?max-results=50&alt=json
+		
+		
+		#below tells plugin about the views                
 def setView(content, viewType):
         # set content type so library shows more views and info
         if content:
@@ -244,6 +356,8 @@ elif mode==10:
         YOUsubs(url)
 elif mode==11:
         YOULinkAll(url)
+elif mode==12:
+        PlayPlayList(url)
        
        
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
