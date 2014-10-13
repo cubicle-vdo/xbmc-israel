dcc4f8e23d7fc178cd27c367a5abc4f4c3434d2a
avigdork <avigdork@hotmail.com>
2014-10-14 00:37:09 +0300

    IsraeLIVE 4.0.3
    - TV-Guides downloading as zip-files
    - TV-Guide for Israeli's channels.
    - It's now showing TV-Guide at favorites too.

--- plugin.video.israelive/service.py
+++ plugin.video.israelive/service.py
@@ -1,6 +1,8 @@
 import xbmc, xbmcaddon, xbmcgui, os, sys
 
 Addon = xbmcaddon.Addon()
+AddonName = Addon.getAddonInfo("name")
+icon = Addon.getAddonInfo('icon')
 libDir = os.path.join(Addon.getAddonInfo("path").decode("utf-8"), 'resources', 'lib')
 sys.path.insert(0, libDir)
 import common, myFilmon
@@ -9,11 +11,10 @@ user_dataDir = xbmc.translatePath(Addon.getAddonInfo("profile")).decode("utf-8")
 if not os.path.exists(user_dataDir):
 	os.makedirs(user_dataDir)
 
-plxListFile = os.path.join(user_dataDir, 'plxList.txt')
-plxListFileUrl = "https://dl.dropboxusercontent.com/u/94071174/israelive/SUB/plxList.txt"
-	
-AddonName = Addon.getAddonInfo("name")
-icon = Addon.getAddonInfo('icon')
+remoteSettingsFile = os.path.join(user_dataDir, "remoteSettings.txt")
+remoteSettingsUrl = "https://dl.dropboxusercontent.com/u/94071174/israelive/SUB/remoteSettings.txt"
+globalGuideFile = os.path.join(user_dataDir, "guide.txt")
+checkInterval = 6
 	
 def sleepFor(timeS):
     while((not xbmc.abortRequested) and (timeS > 0)):
@@ -21,41 +22,46 @@ def sleepFor(timeS):
         timeS -= 1
 		
 def CheckUpdates():
+	remoteSettings = common.GetUpdatedList(remoteSettingsFile, remoteSettingsUrl)
+	if remoteSettings == []:
+		return
+		
+	global checkInterval
+	try:
+		checkInterval = remoteSettings["checkInterval"] * 3600 # in hours
+	except:
+		pass
+		
 	if Addon.getSetting("saveFilmonEPG") == "false":
 		return
 		
+	common.UpdateZipedFile(globalGuideFile, remoteSettings["globalGuide"]["url"])
+	
 	plxType = int(Addon.getSetting("PlxPlaylist"))
 	if plxType == 0:
-		PlxPlaylist = "zip"
+		package = remoteSettings["packages"]["zip"]
 		guideFile = os.path.join(user_dataDir, 'filmonZipGuide.txt')
 	elif plxType == 1:
-		PlxPlaylist = "light"
+		package = remoteSettings["packages"]["light"]
 		guideFile = os.path.join(user_dataDir, 'filmonLightGuide.txt')
 	else:
-		PlxPlaylist = "full"
+		package = remoteSettings["packages"]["full"]
 		guideFile = os.path.join(user_dataDir, 'filmonFullGuide.txt')
 	
-	plxList = common.ReadPlxList(plxListFile, plxListFileUrl)
-					
-	isNewGuideFile = common.UpdateFile(guideFile, plxList[PlxPlaylist]["guide"])
+	isNewGuideFile = common.UpdateZipedFile(guideFile, package["guide"])
 	if isNewGuideFile:
 		return
 		
 	print "{0}: Updating filmonGuide localy.".format(AddonName)
-	isGuideFileOld = common.isFileOld(guideFile, 24 * 3600) # 24 hours
+	isGuideFileOld = common.isFileOld(guideFile, package["refresh"] * 3600) # 24 hours
 	if isGuideFileOld:
 		try:
 			xbmc.executebuiltin("XBMC.Notification({0}, Making and saving Filmon's guide..., {1}, {2})".format(AddonName, 300000 ,icon))
-			myFilmon.MakePLXguide(plxList[PlxPlaylist]["url"], guideFile)
+			myFilmon.MakePLXguide(package["url"], guideFile)
 			xbmc.executebuiltin("XBMC.Notification({0}, Filmon's guide saved., {1}, {2})".format(AddonName, 5000 ,icon))
 		except:
 			xbmc.executebuiltin("XBMC.Notification({0}, Filmon's guide NOT saved!, {1}, {2})".format(AddonName, 5000 ,icon))
 		
-	
-#if Addon.getSetting("saveFilmonEPG") == "false":
-#	sys.exit()
-	
-checkInterval = 4 * 3600 # 4 hours
 CheckUpdates()
 
 while (not xbmc.abortRequested):
