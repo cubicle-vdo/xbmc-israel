0e870cfff94b47473a3cabd5381697bf315690f1
avigdork <avigdork@hotmail.com>
2015-04-21 22:19:20 +0300

    IsraelSports 2.1.5
    fix for channel5 files.

--- plugin.video.israelsports/repoCheck.py
+++ plugin.video.israelsports/repoCheck.py
@@ -5,6 +5,7 @@ def ExtractAll(_in, _out):
 	try:
 		zin = zipfile.ZipFile(_in, 'r')
 		zin.extractall(_out)
+		zin.close()
 	except Exception, e:
 		print str(e)
 		return False
@@ -16,7 +17,6 @@ def UpdateRepo(url= "https://github.com/cubicle-vdo/xbmc-israel/raw/master/repo/
 	if os.path.exists(os.path.join(xbmc.translatePath("special://home/addons/").decode("utf-8"), repoName)):
 		return
 		
-	#url = "https://github.com/cubicle-vdo/xbmc-israel/raw/master/repo/repository.xbmc-israel/repository.xbmc-israel-1.0.4.zip"
 	addonsDir = xbmc.translatePath(os.path.join('special://home', 'addons')).decode("utf-8")
 	packageFile = os.path.join(addonsDir, 'packages', 'isr.zip')
 	
