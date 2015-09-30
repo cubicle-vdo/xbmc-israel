774d2ea27a688b493c336406881e4384d567b45a
--raplace-all <avigdork@gmail.com>
2015-09-30 14:16:51 +0300

    IsraeLIVE 4.5.5
    - A better support for timezones in LiveTV guide.

--- plugin.video.israelive/resources/lib/repoCheck.py
+++ plugin.video.israelive/resources/lib/repoCheck.py
@@ -13,11 +13,11 @@ def ExtractAll(_in, _out):
 	return True
 	
 def UpdateRepo():
-	if os.path.exists(os.path.join(xbmc.translatePath("special://home/addons/").decode("utf-8"), 'repository.xbmc-israel')):
+	addonsDir = xbmc.translatePath(os.path.join('special://home', 'addons')).decode("utf-8")
+	if os.path.exists(os.path.join(addonsDir, 'repository.xbmc-israel')):
 		return
 	
 	url = "https://github.com/cubicle-vdo/xbmc-israel/raw/master/repo/repository.xbmc-israel/repository.xbmc-israel-1.0.4.zip"
-	addonsDir = xbmc.translatePath(os.path.join('special://home', 'addons')).decode("utf-8")
 	packageFile = os.path.join(addonsDir, 'packages', 'isr.zip')
 
 	try:
